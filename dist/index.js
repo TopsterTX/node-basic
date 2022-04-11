a8ae02a88fe75ff095dec4e78ee9481bcba153cf
Ilya Solomahin <ilya.solomahin@simbirsoft.com>
2022-04-11 23:36:17 +0300

    [master] created express instance app

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,11 @@
 "use strict";
-const a = 10;
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+const PORT = 7000;
+app.listen(PORT, () => {
+    console.log('Hi');
+});
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-const a: number = 10
+import express from 'express';
+
+const app = express();
+const PORT = 7000
+
+app.listen(PORT, () => {
+    console.log('Hi');
+})
